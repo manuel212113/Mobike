35e6bbfffc1f92d1ae6422b5f89c768fe8191e17
manuelToro <manueltoro212113@gmail.com>
2020-11-06 22:18:14 -0300

    Mapa Listo

--- script.js
+++ script.js
@@ -4,20 +4,35 @@
   const close = document.querySelector(".close");
  const ingresarBtn=document.querySelector("#IngresarBtn");
 
+
  
-
-
   window.addEventListener("load",function(){
   
+  
+
+
+
+
+
+  })
+
+
+
+  
+
+
+
+
+
+
+function showMap(){
+
     var map= L.map('mapita').setView([-33.694113,-71.214489],13);
 
- L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', { 
+ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
    attribution:' <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
  }).addTo(map);
-   // setTimeout(function(){
-   //   loginPopup.classList.add("show");
-   // },5000)
- 
+  
 
   let iconMarker = L.icon({
     iconUrl: 'https://cdn.mapmarker.io/api/v1/pin?size=178&background=%23FE9200&icon=fa-bicycle&color=%23FFFFFF&voffset=0&hoffset=1&',
@@ -27,10 +42,20 @@
     
 })
 
-let marker2 = L.marker([-33.694113,-71.214489], { icon: iconMarker }).bindPopup('Estacion Duoc, Melipilla.').addTo(map)
+let estacion = L.marker([-33.685689,-71.2144432,19], { icon: iconMarker }).bindPopup('Plaza de Armas <br> Bicicletas:10 ').addTo(map)
 
 
-  })
+let estacionDuoc = L.marker([-33.694113,-71.214489], { icon: iconMarker }).bindPopup(' Duoc <br> Bicicletas:12 ').addTo(map)
+
+let estacionLider = L.marker([-33.6808439,-71.2039486,21], { icon: iconMarker }).bindPopup('Lider <br> Bicicletas:30 ').addTo(map)
+
+
+
+  
+
+
+
+}
 
   function showPopup(){
         const timeLimit = 5 // seconds;
